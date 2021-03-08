23dc814a1c1d2f17321cfc2aacb808434bd538da
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2021-03-08 23:54:50 +0900

    updated main-screen w/ folding motion
    add folding motion on ToDoList div. w/ Click event.

--- js/main-screen.js
+++ js/main-screen.js
@@ -1,5 +1,22 @@
 const input = document.querySelector(".profile__name");
 const USER_LS = "currentUser";
+const arrow = document.getElementById("down__btn");
+const box = document.querySelector(".main__box");
+const UNFOLD = "unfold";
+
+
+function handleFold(event){
+    event.preventDefault();
+    if(box.classList.contains(UNFOLD)){
+        box.classList.remove(UNFOLD);
+    }else{
+        box.classList.add(UNFOLD);
+    }
+}
+
+function foldMenu(){
+    arrow.addEventListener("click",handleFold);
+}
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
@@ -9,5 +26,6 @@ function loadName(){
 
 function init(){
     loadName();
+    foldMenu();
 }
 init();
